b33393d65f0074b89c8d29a9e57830da855c0a00
marcosmororo <87822940+marcosmororo@users.noreply.github.com>
2026-04-22 10:06:19 -0300

    Update diagnostico_windows.bat

--- pages/diagnostico_windows.bat
+++ pages/diagnostico_windows.bat
@@ -1,34 +1,50 @@
 @echo off
 title Diagnostico NOC PRO - Windows
+mode con: cols=80 lines=25
 color 0b
-set FILE=resultado_diagnostico.txt
 
+set FILE=resultado_diagnostico_win.txt
+
+echo ===================================================
+echo    DIAGNOSTICO DE REDE NOC - INICIANDO...
+echo ===================================================
+
+echo Gerando relatorio... aguarde.
 echo ======================================== > %FILE%
 echo RELATORIO DE REDE - WINDOWS >> %FILE%
 echo Data/Hora: %date% %time% >> %FILE%
 echo ======================================== >> %FILE%
 
-echo Executando Ping (Google DNS)...
+echo 1. VERIFICANDO IP PUBLICO...
+echo [IP PUBLICO] >> %FILE%
+for /f "delims=" %%a in ('curl -s https://ifconfig.me') do set IP=%%a
+echo Seu IP Publico e: %IP%
+echo IP Publico: %IP% >> %FILE%
+
+echo 2. TESTANDO LATENCIA (Google)...
 echo [PING 8.8.8.8] >> %FILE%
 ping 8.8.8.8 -n 5 >> %FILE%
 
-echo Executando DNS Lookup...
+echo 3. TESTANDO DNS...
 echo [NSLOOKUP] >> %FILE%
 nslookup google.com >> %FILE%
 
-echo Executando Rota (Traceroute)...
+echo 4. RASTREANDO ROTA (Pode demorar)...
 echo [TRACEROUTE] >> %FILE%
 tracert -d -h 15 8.8.8.8 >> %FILE%
 
-echo Testando Tempo de Abertura (Web)...
-echo [CURL HTTP] >> %FILE%
-powershell -Command "(Measure-Command {Invoke-WebRequest -Uri 'http://google.com' -Method Head}).TotalMilliseconds" >> %FILE%
+echo 5. VELOCIDADE DE ABERTURA HTTP...
+echo [HTTP RESPONSE TIME] >> %FILE%
+powershell -Command "$t = Measure-Command { iwr -Uri http://www.google.com -Method Head }; $t.TotalMilliseconds" >> %FILE%
 echo ms >> %FILE%
 
+echo ======================================== >> %FILE%
+echo FIM DO DIAGNOSTICO >> %FILE%
+
 echo.
-echo ========================================
-echo TESTE CONCLUIDO!
-echo O arquivo 'resultado_diagnostico.txt' foi gerado.
-echo Envie este arquivo para o suporte tecnico.
-echo ========================================
+echo ===================================================
+echo CONCLUIDO!
+echo Arquivo gerado: %FILE%
+echo Envie este arquivo para o suporte.
+echo ===================================================
 pause
